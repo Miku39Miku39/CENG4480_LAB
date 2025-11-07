c5c9b5a71528128e94f5314a09334c2abf65825f
Miku39Miku39 <jackytong20031228@gmail.com>
2025-11-07 20:01:21 +0800

    Fixed frame size of camera

--- Lab4/lab4-camera_sample_code/lab4_video.py
+++ Lab4/lab4-camera_sample_code/lab4_video.py
@@ -2,6 +2,12 @@ import cv2
 import datetime
  
 cap = cv2.VideoCapture(0)
+
+desired_width = 640
+desired_height = 480
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, desired_width)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, desired_height)
+
 frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 word_x = int(frame_width / 10)
@@ -13,7 +19,8 @@ while (cap.isOpened()):
     cv2.putText(frame, time_text, (word_x,word_y),
                 cv2.FONT_HERSHEY_SIMPLEX,1,(55,255,155),2)
     print(frame.shape)
-    cv2.imshow("real_time",frame)
+    resized_frame = cv2.resize(frame, (desired_width, desired_height))
+    cv2.imshow("real_time",resized_frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
  
